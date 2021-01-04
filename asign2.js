9e204c296fd4a16b72c97b86df5d2bd808f3ed87
Amit1199 <72022197+Amit1199@users.noreply.github.com>
2021-01-04 23:04:36 +0530

    Add files via upload

---
+++ asign2.js
@@ -0,0 +1,21 @@
+//QUE 2 a)
+console.log("Hello Edac")
+
+//QUE 2 b)
+var w=true;
+var x=10;
+var y="EDAC";
+var emp=["Amit","Aniruddha","Aniket"]
+stu={id:101,name:"Sir",fee:50000}
+
+console.log(w + ","+ x +"," + y + "," + emp +","+stu.id+","+stu.name+","+stu.fee );
+
+// QUE 2 c)
+var str="Welcome to cdac ";
+str.charAt(3);
+str.concat("Mumbai");
+str.substr(2,4);
+str.toUpperCase();
+str.split(" ");
+str.trim();
+
